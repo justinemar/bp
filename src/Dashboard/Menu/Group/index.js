411a0f95cea261b7fa15233f641bf18386d359a6
Justine Cantado <justinecantado@gmail.com>
2018-05-28 05:06:40 +0000

    reword comment

--- src/Dashboard/Menu/Group/index.js
+++ src/Dashboard/Menu/Group/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 class MenuGroup extends React.Component{
     render(){
         return (
-            {/* Build MenuGroup Following the structure of the Profile and Setting Tab */}    
+            {/* Leave this empty for now */}    
         )
     }
 }
