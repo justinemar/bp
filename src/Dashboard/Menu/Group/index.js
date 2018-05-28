177b76eb7d4e2197d174da5b71ea545f4221510b
Justine Cantado <justinecantado@gmail.com>
2018-05-28 04:50:42 +0000

    Init group tab

---
+++ src/Dashboard/Menu/Group/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+class MenuGroup extends React.Component{
+    render(){
+        return (
+            {/* Build MenuGroup Following the structure of the Profile and Setting Tab */}    
+        )
+    }
+}
+
+
+
+export default MenuGroup;
