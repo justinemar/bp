e43c8d0f07a3068802c2aec51677ba36fcbec4d0
Justine Cantado <justinecantado@gmail.com>
2018-06-11 06:02:27 +0000

    update route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,20 +3,14 @@ const router = express.Router();
 const root = require("../controller/IndexController");
 const account = require("../controller/AccountController");
 const comment = require("../controller/CommentController");
-const status = require("../controller/PostController");
 const helper = require("../utils/lib/account");
 
 
 router.get('/', root.index);
 
-
-
-
 router.post('/register', helper.checkUser, account.user_register);
 
 router.post('/login', account.user_login);
-
-
 
 router.post('/comment', helper.verifyToken, comment.new);
 
