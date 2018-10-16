66d41a6f79d9a4a2c992e666f236a39bb2842978
justinemar <justinecantado@gmail.com>
2018-10-16 21:46:23 +0800

    tweaks

--- src/utils/authService.js
+++ src/utils/authService.js
@@ -87,12 +87,12 @@ export default class AuthService {
 
     _checkStatus(response) {
         // raises an error in case response status is not a success
-        if (response.status >= 200 && response.status < 300) { // Success status lies between 200 to 300
+        if (response.code >= 200 && response.code < 300) { // Success status lies between 200 to 300
             return response
         } else {
             var error = new Error(response.statusText)
             error.response = response
-            throw error
+            return false;
         }
     }
     
--- src/utils/withAuth.js
+++ src/utils/withAuth.js
@@ -31,7 +31,7 @@ export default function withAuth(AuthComponent) {
                     message: null,
                     type: null,
                     code: null
-                  },
+                  }
             };
         }
         componentWillMount() {
