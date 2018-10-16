97a5be82f2567505d44518a45c302c3b1e722b40
justinemar <justinecantado@gmail.com>
2018-10-16 21:37:52 +0800

    add twitch key

--- server/utils/lib/config.js
+++ server/utils/lib/config.js
@@ -2,8 +2,12 @@ const cloudinary = require("cloudinary");
 require('dotenv').config();
 
 
-exports.config = cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-});
+exports.config = {
+    cloudinary: 
+        cloudinary.config({
+            cloud_name: process.env.CLOUD_NAME,
+            api_key: process.env.CLOUD_API_KEY,
+            api_secret: process.env.CLOUD_API_SECRET
+        }), 
+    twitch_client_id: process.env.TWITCH_CLIENT_ID
+}
