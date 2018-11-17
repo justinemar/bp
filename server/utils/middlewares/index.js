12c0ebc17edbca6ac4c18124bc3d5fcbb9f08e53
justinemar <justinecantado@gmail.com>
2018-11-17 19:42:09 +0800

    remove unnecessary middleware, format tweaks

--- server/utils/middlewares/index.js
+++ server/utils/middlewares/index.js
@@ -105,23 +105,6 @@ module.exports = {
         });
     },
 
-    verifyToken: (req, res, next) => {
-        const token = req.headers.authorization.slice(7 - req.headers.authorization.length);
-        jwt.verify(token, process.env.KEY1, (err, decoded) => {
-            if (err) {
-                res.status(401).json({
-                    message: 'Your session has expired, please login to continue where you left off',
-                    type: 'error',
-                    code: 401,
-                });
-            }
-
-            if (decoded) {
-                next();
-            }
-        });
-    },
-
     checkUserStatus: (req, res, next) => {
         Account.findOne({ user_email: req.body.email })
         .exec((err, user) => {
