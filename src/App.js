2e473886cf0c5d6c7fa3f761d02a051bb6b5251a
justinemar <justinecantado@gmail.com>
2018-11-27 01:32:29 +0800

    linting, tweaks , update package

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import fontawesome from '@fortawesome/fontawesome';
 import { faSearchengin } from '@fortawesome/fontawesome-free-brands';
 import {
  faSignOutAlt, faEllipsisH, faPlay, faEye, faSave, faBan, faEdit, faImage, faNewspaper,
- faSlidersH, faBell, faFistRaised, faCrown, faAngleDoubleLeft, faBars, faTimes,
+ faSlidersH, faBell, faFistRaised, faCrown, faAngleDoubleLeft, faBars, faTimes, faSpinner,
+ faTimesCircle,
 } from '@fortawesome/free-solid-svg-icons';
 import { Route, Switch } from 'react-router-dom';
 import RootPage from './Root';
 import Dashboard from './Dashboard';
-import Verify from './Verify';
 
-fontawesome.library.add(faSearchengin, faTimes, faBars, faAngleDoubleLeft, faCrown, faSignOutAlt, faPlay, faEye, faSave, faBan, faEdit, faImage, faNewspaper, faSlidersH, faBell, faFistRaised, faEllipsisH);
+fontawesome.library.add(faTimesCircle, faSpinner, faSearchengin, faTimes, faBars, faAngleDoubleLeft, faCrown, faSignOutAlt, faPlay, faEye, faSave, faBan, faEdit, faImage, faNewspaper, faSlidersH, faBell, faFistRaised, faEllipsisH);
 class App extends React.Component {
     render() {
         return (
           <div>
             <Switch>
               <Route exact path="/" render={props => <RootPage {...props} />} />
-              <Route path="/verifyEmail/:token" render={props => <Verify {...props} />} />
               <Route path="/dashboard" render={props => <Dashboard {...props} />} />
             </Switch>
           </div>
